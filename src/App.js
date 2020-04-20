3903ee10278c571aded2d9568412b4e65403e6f2
tlb <1004333142@qq.com>
2020-04-20 15:05:04 +0800

    add all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LayOut from './layout/index'
+
+function App() {
+  return (
+    <div className="App">
+      <LayOut></LayOut>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Page from '../component/page'
+
+export default class LayOut extends Component {
+    render() {
+        return (
+            <div>
+             <Page></Page>
+            </div>
+        )
+    }
+}
